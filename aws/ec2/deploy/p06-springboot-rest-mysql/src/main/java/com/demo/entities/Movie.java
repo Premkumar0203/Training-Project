586bec003555a58cff92ee75bee1cdde52cf4b24
Arun Kumar <arunkumar31786@gmail.com>
2022-08-10 14:53:41 +0530

    new jar

---
+++ aws/ec2/deploy/p06-springboot-rest-mysql/src/main/java/com/demo/entities/Movie.java
@@ -0,0 +1,59 @@
+package com.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String director;
+	private double ratings;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public double getRatings() {
+		return ratings;
+	}
+	public void setRatings(double ratings) {
+		this.ratings = ratings;
+	}
+	
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", title=" + title + ", director=" + director + ", ratings=" + ratings + "]";
+	}
+	public Movie() {
+	}
+	public Movie(int id, String title, String director, double ratings) {
+		this(title, director, ratings);
+		this.id = id;
+	}
+	public Movie(String title, String director, double ratings) {
+		super();
+		this.title = title;
+		this.director = director;
+		this.ratings = ratings;
+	}	
+}
---
+++ aws/ec2/deploy/p06-springboot-rest-mysql/src/main/java/com/demo/services/MovieService.java
@@ -0,0 +1,81 @@
+package com.demo.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.entities.Movie;
+import com.demo.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repo;
+
+	public List<Movie> findAll() {
+		return repo.findAll();
+	}
+	
+	public Movie findOneById(int id) {
+		Optional<Movie> optional = repo.findById(id);
+		if(optional.isEmpty()) {
+			throw new RuntimeException("Could not find movie with id: "+id);
+		} else {
+			return optional.get();
+		}
+	}
+	
+	public Movie createNewMovie(Movie m) {
+		return repo.save(m);
+	}
+	
+	public Movie updateMovie(int id, Movie m) {
+		Movie dbMovie = findOneById(id);
+		if(m.getTitle()!=null) {
+			dbMovie.setTitle(m.getTitle());
+		}
+		if(m.getDirector()!=null) {
+			dbMovie.setDirector(m.getDirector());
+		}
+		if(m.getRatings()>0) {
+			dbMovie.setRatings(m.getRatings());
+		}
+		
+		return repo.save(dbMovie);
+	}
+	
+	public Movie deleteMovie(int id) {
+		Movie dbMovie = findOneById(id);
+
+//		repo.deleteById(id);
+		repo.delete(dbMovie);
+
+		return dbMovie;
+		
+	}
+
+	public List<Movie> findByTitle(String title) {
+		if(title!=null) {			
+			return repo.findByTitle(title);
+		} else {
+			return new ArrayList<Movie>();
+		}
+	}
+	
+	public List<Movie> findByTitleAndRating(String title, double rating) {
+	
+		if(rating>0) {
+			return repo.findByTitleAndRating(title, rating);			
+		} else {
+			return repo.findByTitle(title);
+		}
+		
+	}
+	
+	
+
+}
